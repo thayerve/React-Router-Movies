e525b5113b63be85c69238151b7925cb1b035750
Virginia Thayer <virginia@stripe.com>
2019-07-23 15:23:01 -0700

    Added link to Home button in SavedList.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <Link className="home-button" to="/">Home</Link>
+    </div>
+  );
+}
 
 export default SavedList;
